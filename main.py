6c0ae20dfd041afe6aef56126edfb96f957940ae
muhibbx5 <muhibbali56@gmail.com>
2024-10-25 10:27:00 +0500

    commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# Input a number
+number = int(input("Enter a number: "))
+
+# Check if the number is odd or even
+if number % 2 == 0:
+    print(f"{number} is Even")
+else:
+    print(f"{number} is Odd")
